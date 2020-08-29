500043d5be48efefc22e74f6847f3f3b7a340227
trantiendat1496 <51437420+trantiendat1496@users.noreply.github.com>
2020-08-29 23:16:47 +0700

    first commit

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
 
-const productRoutes = require('./routes/products.route') 
+var express = require('express');
+var bodyPaser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var path = require('path');
 
-
-
-
-const port = 9000;
-
-
-
-app.set('view engine', 'pug')
-app.set('views', './views')
-
-app.use(express.static('public'));
+var  mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOOSE_URL);
 
 
-app.get('/', (req, res) => {
-    res.render('index');
-})
+var authRoute = require('./routes/auth.route');
+var cartRoute = require('./routes/cart.route');
+var adminRoute = require('./routes/admin.route');
+var homeRoute = require('./routes/home.route');
+
+var authMiddleware = require('./middlewares/auth.middleware');
+var sessionMiddleware = require('./middlewares/session.middleware');
+
+var port = 30;
+
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyPaser.json()) // for parsing application/json
+app.use(bodyPaser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(cookieParser(process.env.SESSION_SECRET));
+
+app.use(sessionMiddleware.session);
+
+const api_key = process.env.SENDGRID_API_KEY;
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/admin', express.static(path.join(__dirname, '/public')));
+app.use('/auth', express.static(path.join(__dirname, '/public')));
+app.use('/books/search', express.static(path.join(__dirname, '/public')));
+app.use('/admin/updateUser', express.static(path.join(__dirname, '/public')));
+app.use('/admin/updateBook', express.static(path.join(__dirname, '/public')));
 
 
 
+app.use('/', homeRoute);
+app.use('/auth', authRoute);
+app.use('/cart', cartRoute);
+app.use('/admin',authMiddleware.requireAuth, adminRoute);
 
-app.use('/products', productRoutes);
+
+app.listen(port, function(){
+	console.log('Server listenning on port ' + port);
+});
 
 
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at ` + port)
-})
